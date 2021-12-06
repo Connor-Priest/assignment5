77dabdcc1c4425bfeb6804e3ae755e114ebc6810
Connor-Priest <cpriest965@g.rwu.edu>
2021-12-06 13:29:00 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def function_1():
+    name = input("What is your name? : ")
+    print('Hello{name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
